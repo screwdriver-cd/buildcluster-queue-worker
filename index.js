1a618612ab3260d2f9439ab21c46d7a54178bebd
imiyagam <imiyagam@yahoo-corp.jp>
2022-01-18 15:51:23 +0900

    fix: wait for connection

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
-const conf = require('config');
-const httpdConfig = conf.get('httpd'); // Setup HTTPd
-const server = require('./lib/server');
-const receiver = require('./receiver');
-const connection = receiver.listen();
+(async() => {
+    const conf = require('config');
+    const httpdConfig = conf.get('httpd'); // Setup HTTPd
+    const server = require('./lib/server');
+    const receiver = require('./receiver');
+    const connection = await receiver.listen();
 
-server(httpdConfig, connection);
+    server(httpdConfig, connection);
+})();
+
