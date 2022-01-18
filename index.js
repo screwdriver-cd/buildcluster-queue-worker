7b621edd0175606068be30e8e8662abe33ac3566
Ibuki <ibu1224@users.noreply.github.com>
2022-01-19 05:32:56 +0900

    fix(2491): health check processing error (#141)

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const conf = require('config');
 const httpdConfig = conf.get('httpd'); // Setup HTTPd
 const server = require('./lib/server');
 const receiver = require('./receiver');
-const connection = receiver.listen();
 
-server(httpdConfig, connection);
+(async () => {
+    const connection = await receiver.listen();
+
+    server(httpdConfig, connection);
+})();
