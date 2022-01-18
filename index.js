a861a6222e8acc3335d05ac53d9708a5b3b55a16
imiyagam <imiyagam@yahoo-corp.jp>
2022-01-18 16:15:18 +0900

    fix: lint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-(async() => {
-    const conf = require('config');
-    const httpdConfig = conf.get('httpd'); // Setup HTTPd
-    const server = require('./lib/server');
-    const receiver = require('./receiver');
+const conf = require('config');
+const httpdConfig = conf.get('httpd'); // Setup HTTPd
+const server = require('./lib/server');
+const receiver = require('./receiver');
+
+(async () => {
     const connection = await receiver.listen();
 
     server(httpdConfig, connection);
 })();
-
